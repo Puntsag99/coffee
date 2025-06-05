f6d9810b8f9ca0c4525f7d7d10e0dfcdb50c8053
Missile <missile@example.com>
2025-06-05 21:02:06 +0800

    gg

---
+++ app/components/profile/Payment.tsx
@@ -0,0 +1,10 @@
+export const Payment = () => {
+  return (
+    <div className="w-[510px] flex flex-col gap-y-12">
+      <div className="flex flex-col gap-y-[6px]">
+        <h3 className="text-2xl font-normal">How would you like to be paid?</h3>
+        <p>fa</p>
+      </div>
+    </div>
+  );
+};
--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
-  },
-  prisma: {
-    schema: "app/prisma/schema.prisma",
-  },
+  }
+ 
 };
 
 export default nextConfig;
